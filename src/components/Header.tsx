c96f13eefb9478d3bf915f807a1ca3b086450cdb
Kizuki <aikikizuki@gmail.com>
2025-10-14 08:51:08 +0900

    feat: gsap追加、メインページにアニメーションを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 export default function Header() {
   return (
-    <div className="flex justify-between py-4 px-8 blur-bg font-fugaz">
-      <h1>Kz Creation</h1>
+    <header className="flex items-center justify-between py-4 px-8 blur-bg fixed top-0 left-0 right-0 text-white">
+      <h1 className="text-3xl font-fugaz">Kz Creation</h1>
       <div className="flex gap-4">
         Menu there
       </div>
-    </div>
+    </header>
   )
 }
