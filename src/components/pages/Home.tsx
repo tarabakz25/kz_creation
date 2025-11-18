9ff4a86deccb7b5610a00f62b9b094ce6297e6a2
Kizuki <aikikizuki@gmail.com>
2025-11-18 10:14:12 +0900

    feat: worksのモックを追加

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home: Rect.FC = () => {
+const Home: React.FC = () => {
   return (
     <div className="w-full h-screen flex items-center p-12 main-fg font-futura">
       <h1 className="text-[8rem] italic">I'm a seeker who thrives in chaotic times!</h1>
--- src/components/pages/Works.tsx
+++ src/components/pages/Works.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap';
 
 const Works: React.FC = () => {
+  const contantRef = useRef<HTMLDivElement>(null);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold mb-8">Works Page</h1>
